ab5ba76d2e11e5b756cf278d4c9a7878b7c58be2
adibfiroz <adibfiroz9702@gmail.com>
2024-07-20 12:40:12 +0530

    window error fix

--- app/(pages)/(routes)/schedule/page.tsx
+++ app/(pages)/(routes)/schedule/page.tsx
@@ -1,10 +1,13 @@
+import ClientOnly from '@/components/ClientOnly'
 import Schedule from '@/components/schedule'
 import React from 'react'
 
 const SchedulePage = () => {
     return (
         <div className=' max-w-screen-lg mx-auto px-3 pt-36 mb-28'>
-            <Schedule />
+            <ClientOnly>
+                <Schedule />
+            </ClientOnly>
         </div>
     )
 }
