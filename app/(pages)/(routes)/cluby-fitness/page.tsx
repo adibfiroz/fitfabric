4e605bc71e0a243ac17150c3ec128c1653b622bf
adibfiroz <adibfiroz9702@gmail.com>
2024-07-20 12:42:23 +0530

    cluby fitness errror

--- app/(pages)/(routes)/cluby-fitness/page.tsx
+++ app/(pages)/(routes)/cluby-fitness/page.tsx
@@ -1,10 +1,13 @@
+import ClientOnly from "@/components/ClientOnly"
 import Schedule from "@/components/schedule"
 
 const ClubyFitness = () => {
 
     return (
         <div className=' max-w-screen-lg mx-auto px-3 pt-36 mb-28'>
-            <Schedule />
+            <ClientOnly>
+                <Schedule />
+            </ClientOnly>
         </div>
     )
 }
